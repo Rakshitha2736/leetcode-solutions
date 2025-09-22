4c109f3009a1b8f6bdde7be535d8c644d495521f
Rakshitha2736 <155134540+Rakshitha2736@users.noreply.github.com>
2025-09-22 11:50:33 +0530

    [1137] [Time Beats: 100.00%] [Memory Beats: 37.32%] - LeetPush

--- N-th-Tribonacci-Number.cpp
+++ N-th-Tribonacci-Number.cpp
@@ -6,9 +6,12 @@ public:
           return n;
         if(n==2)
           return 1;
-        if(dp[n]!=-1)
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=1;
+        for(int i=3;i<=n;i++)
+         dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
           return dp[n];
-        return dp[n]=solve (n-1,dp)+solve(n-2,dp)+solve(n-3,dp);
     }
     int tribonacci(int n) {
         vector<int> dp(n+1,-1);
